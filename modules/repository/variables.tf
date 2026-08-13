a4f897b5c19726f009c52884889624c47d760bff
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2026-08-13 15:19:34 +0100

    [PP-15183] Allow bypass of PR requirement

--- modules/repository/variables.tf
+++ modules/repository/variables.tf
@@ -4,15 +4,16 @@ variable "name" {
 
 variable "repository" {
   type = object({
-    description      = optional(string, null)
-    visibility       = string
-    homepage_url     = optional(string, null)
-    default_branch   = optional(string, "main")
-    has_discussions  = optional(bool, false)
-    has_issues       = optional(bool, true)
-    license_template = optional(string, null)
-    topics           = optional(list(string), [])
-    actions_enabled  = optional(bool, true)
-    actions_allowed  = optional(list(string), [])
+    description        = optional(string, null)
+    visibility         = string
+    homepage_url       = optional(string, null)
+    default_branch     = optional(string, "main")
+    has_discussions    = optional(bool, false)
+    has_issues         = optional(bool, true)
+    license_template   = optional(string, null)
+    topics             = optional(list(string), [])
+    actions_enabled    = optional(bool, true)
+    actions_allowed    = optional(list(string), [])
+    allow_push_to_main = optional(bool, false)
   })
 }
